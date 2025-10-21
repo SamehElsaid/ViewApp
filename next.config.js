30529c80381286669b018ac3bed6ade8d66bfca8
Sameh Elmeligy <109442419+SamehElsaid@users.noreply.github.com>
2025-10-21 13:03:37 +0300

    Update environment variables, enhance error handling in axios calls, and add console logs for debugging in various components

--- next.config.js
+++ next.config.js
@@ -9,10 +9,9 @@ module.exports = withTM({
     defaultLocale: 'en',
     localeDetection: true
   },
-
   env: {
     API_URL: 'https://lowcodetest-ayeuaucehyerfves.uaenorth-01.azurewebsites.net/api',
-    IDENTITY_URL: 'https://localhost:7000/',
+    IDENTITY_URL: 'https://identityserver-afbzgwf7eva5gdhe.uaenorth-01.azurewebsites.net/',
     DEV_MODE: false,
     DOMAIN: 'https://view-app-omega.vercel.app/'
   },
