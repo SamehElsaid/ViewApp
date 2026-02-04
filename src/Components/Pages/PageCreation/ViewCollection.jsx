85b63115369bf18e7814080da77095172a7281fd
Sameh Elmeligy <109442419+SamehElsaid@users.noreply.github.com>
2026-02-04 19:03:41 +0200

    fix error

--- src/Components/Pages/PageCreation/ViewCollection.jsx
+++ src/Components/Pages/PageCreation/ViewCollection.jsx
@@ -487,7 +487,14 @@ export default function ViewCollection({
         : `generic-entities/${data.collectionName}/?pageId=${pageId}${requestId ? `&requestId=${requestId}` : ''}`
 
     if (entitiesId && collectionName) {
-
+      if (data.onSubmit) {
+        const evaluatedFn = eval('(' + data.onSubmit + ')')
+        if (handleSubmitEvent) {
+          handleSubmitEvent()
+        } else {
+          evaluatedFn()
+        }
+      }
       axiosPut(
         `generic-entities/${collectionName}?Id=${entitiesId}&requestId=${requestId}&pageId=${pageId}`,
         locale,
@@ -497,22 +504,7 @@ export default function ViewCollection({
           if (res.status) {
             setReload(prev => prev + 1)
             toast.success(messages.dialogs.dataSentSuccessfully)
-            if (res.status) {
-              setReload(prev => prev + 1)
-              toast.success(messages.dialogs.dataSentSuccessfully)
-              if (data.onSubmit) {
-                const evaluatedFn = eval('(' + data.onSubmit + ')')
-                if (handleSubmitEvent) {
-                  handleSubmitEvent()
-                } else {
-                  evaluatedFn()
-                }
-              }
-
-              if (data?.redirect) {
-                push(`/${locale}/${data?.redirect === '/' ? '' : data?.redirect}`)
-              }
-            }
+           
             if (data?.redirect) {
               push(`/${locale}/${data?.redirect === '/' ? '' : data?.redirect}`)
             }
